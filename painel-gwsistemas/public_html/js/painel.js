be3aab12f6f7278df22784f010b7e7497f8b5b76
MateusVeloso <mateusveloso.developer@gmail.com>
2018-10-15 19:47:08 -0300

    criando projeto

--- painel-gwsistemas/public_html/js/painel.js
+++ painel-gwsistemas/public_html/js/painel.js
@@ -1,2 +1,6 @@
 //mudar cor de variavel
 //document.documentElement.style.setProperty('--bg-nav', 'red');;
+$(function(){
+    $('#input-cor-menu').val($('.nav-menu-lateral').css('background'));
+    $('#input-cor-caixas').val($('.container-dados').css('background'));
+});
